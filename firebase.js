3ec893004621b71900e129ccd473946f9cea8602
Vy <vykl.do04@gmail.com>
2024-08-04 11:54:24 -0400

    import firebase

--- firebase.js
+++ firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/compat/app";
-import {getFirestore} from "firebase/compat/firestore";  
+import firebase from "firebase/compat/app";
+// Required for side-effects
+import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";  
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
